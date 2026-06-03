c6d1704c7315aceb7174c708cba2e76bb030e583
Joe Watson <jw120@users.noreply.github.com>
2026-06-03 19:47:55 +0800

    started test move

--- walk_through/evaluation.rkt
+++ walk_through/evaluation.rkt
@@ -1,5 +1,4 @@
 #lang plait
-(print-only-errors #true)
 
 ;;
 ;; Parse
@@ -19,21 +18,6 @@
          [(symbol=? f '+) (plusE (parse x) (parse y))]
          [else (error 'parse "unrecognized symbol applied")]))]
     [else (error 'parse "unrecognized form in s-expression")]))
-
-(module+ test
-  (test (parse `1) (numE 1))
-  (test (parse `2.3) (numE 2.3))
-  (test (parse `{+ 1 2}) (plusE (numE 1) (numE 2)))
-  (test (parse `{+ 1 {+ {+ 2 3} 4}})
-        (plusE (numE 1)
-               (plusE (plusE (numE 2) (numE 3))(numE 4))))
-  (test (parse `#f) (boolE #f))
-  (test (parse `#t) (boolE #t))
-  (test (parse `#false) (boolE #f))
-  (test (parse `#true) (boolE #t))
-  (test/exn (parse `z) "cannot parse a symbol")
-  (test/exn (parse `{- 3 2}) "unrecognized symbol")
-  (test/exn (parse `{1 + 2}) "unrecognized form"))
 
 ;;
 ;; Expression
@@ -79,19 +63,6 @@
                          [else (error '+ "expects right hand side to be a number")])]
              [else (error '+ "expects left hand side to be a be a number")]))
 
-(module+ test
-  (test (calc (numE 1)) (numV 1))
-  (test (calc (numE 2.3)) (numV 2.3))
-  (test (calc (plusE (numE 1) (numE 2))) (numV 3))
-  (test (calc (plusE (plusE (numE 1) (numE 2)) (numE 3))) (numV 6))
-  (test (calc (plusE (numE 1) (plusE (numE 2) (numE 4)))) (numV 7))
-  (test (calc (plusE (numE 1) (plusE (plusE (numE 2) (numE 3))(numE 4)))) (numV 10))
-  (test/exn (calc (plusE (numE 1) (boolE #t))) "right hand side")
-  (test/exn (calc (plusE (boolE #f) (boolE #t))) "left hand side")
-  (test (calc (cndE (boolE #f) (numE 2) (numE 3))) (numV 3))
-  (test (calc (cndE (boolE #t) (numE 2) (numE 3))) (numV 2))
-  (test/exn (calc (cndE (numE 2) (numE 2) (numE 3))) "boolean"))
-
 
 ;;
 ;; Run
@@ -100,10 +71,3 @@
 (define (run [s : S-Exp]) : Value
   (calc (parse s)))
 
-(module+ test
-  (test (run `1) (numV 1))
-  (test (run `2.3) (numV 2.3))
-  (test (run `{+ 1 2}) (numV 3))
-  (test (run `{+ {+ 1 2} 3}) (numV 6))
-  (test (run `{+ 1 {+ 2 3}}) (numV 6))
-  (test (run `{+ 1 {+ {+ 2 3} 4}}) (numV 10)))
